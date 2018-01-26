98191dfb941e0a6502f0ed28d6a8b579455b50d0
unknown <Fanor@FDN-WS-023535.acad.dvuadmin.net>
2018-01-25 22:29:29 -0300

    t

---
+++ hello-js-se05-ep02/main.js
@@ -0,0 +1,6 @@
+console.log("Hello you!")
+
+const gibberish = require("./lib2").gibberish // note o './' dando o caminho
+console.log(gibberish("Hello you!"))
+
+// ZmwiiWiOjA8CFPL64+ADb0wl3R2dA+cjZ4SyjJnw3u0=
