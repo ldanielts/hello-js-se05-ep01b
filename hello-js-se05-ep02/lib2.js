203d85e5751ae86670f56a2363a89d1d12a59b9a
unknown <Fanor@FDN-WS-023535.acad.dvuadmin.net>
2018-01-26 20:40:53 -0300

    aula 2

--- hello-js-se05-ep02/lib2.js
+++ hello-js-se05-ep02/lib2.js
@@ -1,7 +1,11 @@
 const CryptoJS = require("crypto-js") // o node acha ele sozinho
-module.exports = {
-  gibberish (word){
-    // https://stackoverflow.com/questions/11889329/word-array-to-string
-    return CryptoJS.SHA256(word).toString(CryptoJS.enc.Base64)
-  }
+
+
+function gibberish (word){
+  // https://stackoverflow.com/questions/11889329/word-array-to-string
+  return CryptoJS.SHA256(word).toString(CryptoJS.enc.Base64)
 }
+
+template = "Oi, %s";
+
+module.exports = { template, gibberish};
--- hello-js-se05-ep02/main.js
+++ hello-js-se05-ep02/main.js
@@ -3,4 +3,49 @@ console.log("Hello you!")
 const gibberish = require("./lib2").gibberish // note o './' dando o caminho
 console.log(gibberish("Hello you!"))
 
+const template = require("./lib2").template;
+console.log(template,'abc');
+
+x = [1,2,3,4,5,6,7,8,9];
+
+x = x.splice(2, 3);
+
+console.log(x);
+
+y = [];
+for (i=1; i <=10; i++) {
+    y.push(i);
+}
+
+console.log(y);
+
+yl = y.map(e => {
+    if (e % 2 == 0) {
+        return e + "p";
+    } else {
+        return e + "i";
+    }
+});
+console.log(yl);
+
+yf = y.map(function poi(e) {
+    if (e % 2 == 0) {
+        return e + "p";
+    } else {
+        return e + "i";
+    }
+});
+console.log(yf);
+
+yt = y.map(e=>(e%2)?e+"i":e+"p")
+console.log(yt);
+
+yf = y.filter(function poi(e) {
+    if (e % 2 == 0) {
+        return e;
+    }
+});
+console.log(yf);
+
+
 // ZmwiiWiOjA8CFPL64+ADb0wl3R2dA+cjZ4SyjJnw3u0=
