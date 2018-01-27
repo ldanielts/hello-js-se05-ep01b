4e6e8068f7065221e3e5dc668043b412b4d4a3fa
unknown <Fanor@FDN-WS-023535.acad.dvuadmin.net>
2018-01-26 21:41:17 -0300

    2

--- hello-js-se05-ep02/main.js
+++ hello-js-se05-ep02/main.js
@@ -28,7 +28,7 @@ yl = y.map(e => {
 });
 console.log(yl);
 
-yf = y.map(function poi(e) {
+yf = y.map(function parOuImpar(e) {
     if (e % 2 == 0) {
         return e + "p";
     } else {
@@ -40,12 +40,28 @@ console.log(yf);
 yt = y.map(e=>(e%2)?e+"i":e+"p")
 console.log(yt);
 
-yf = y.filter(function poi(e) {
+yf = y.filter(function soPar(e) {
     if (e % 2 == 0) {
         return e;
     }
 });
 console.log(yf);
+
+z = [5,1,6,2,7,8,3,4,9,10];
+ys = z.sort((a,b) => a-b);
+console.log(ys);
+
+function  abc(params) {
+    console.log(params + params);
+};
+o = { 
+    abc
+};
+console.log(o);
+
+o.abc('a');;
+
+
 
 
 // ZmwiiWiOjA8CFPL64+ADb0wl3R2dA+cjZ4SyjJnw3u0=
